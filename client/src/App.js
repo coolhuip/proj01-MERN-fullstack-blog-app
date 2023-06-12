3362d8d459c2f072054b2e6a82980eaf87a4467b
Danny <cool.huip@gmail.com>
2023-06-12 14:03:46 -0400

    Make style changes

--- client/src/App.js
+++ client/src/App.js
@@ -13,14 +13,46 @@ function App() {
         </nav>
       </header>
       <div className="post">
-        <img src={postImg_01} alt="d"/>
-        <h2>This is my first test blog post.</h2>
-        <p>
-          Lorem ipsum weather boy. It's gonna rain. The leaves will be green.
-          The sun will shine. Children will be free of technological delusion.
-          Men and women will be free of paranoia. The moon will dance with the
-          waters.
-        </p>
+        <div className="image">
+          <img src={postImg_01} alt="Post 1" />
+        </div>
+        <div className="texts">
+          <h2>This is my first test blog post.</h2>
+          <p>
+            Lorem ipsum weather boy. It's gonna rain. The leaves will be green.
+            The sun will shine. Children will be free of technological delusion.
+            Men and women will be free of paranoia. The moon will dance with the
+            waters.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src={postImg_01} alt="Post 2" />
+        </div>
+        <div className="texts">
+          <h2>This is my first test blog post.</h2>
+          <p>
+            Lorem ipsum weather boy. It's gonna rain. The leaves will be green.
+            The sun will shine. Children will be free of technological delusion.
+            Men and women will be free of paranoia. The moon will dance with the
+            waters.
+          </p>
+        </div>
+      </div>
+      <div className="post">
+        <div className="image">
+          <img src={postImg_01} alt="Post 3" />
+        </div>
+        <div className="texts">
+          <h2>This is my first test blog post.</h2>
+          <p>
+            Lorem ipsum weather boy. It's gonna rain. The leaves will be green.
+            The sun will shine. Children will be free of technological delusion.
+            Men and women will be free of paranoia. The moon will dance with the
+            waters.
+          </p>
+        </div>
       </div>
     </main>
   );
