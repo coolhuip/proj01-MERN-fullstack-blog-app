f1337fa5b728e7c5627733449faa9d73e265fb50
Danny <cool.huip@gmail.com>
2023-06-12 13:38:38 -0400

    Set up nav and content

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,18 @@ import './App.css';
 
 function App() {
   return (
-    <div>
-      test
-    </div>
+    <main>
+      <header>
+        <a href="" className="logo">MyBlog</a>
+        <nav>
+          <a href="">Login</a>
+          <a href="">Register</a>
+        </nav>
+      </header>
+      <div className="entry">
+        <img src="" alt="" />
+      </div>
+    </main>
   );
 }
 
